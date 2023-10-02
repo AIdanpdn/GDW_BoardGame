879fa72ddcfd5f0a02460f0dcd3d680147c59b48
AIdanpdn <144922298+AIdanpdn@users.noreply.github.com>
2023-10-02 16:19:31 -0400

    Updated (oct 2)

--- Assets/Scripts/PawnMove.cs
+++ Assets/Scripts/PawnMove.cs
@@ -6,7 +6,10 @@ public class PawnMove : MonoBehaviour
 {
     public int MoveX;
     public int MoveY;
-
+    public int BlueCardId1 = 0;
+    public int BlueCardId2 = 0;
+    public int BlueCardId3 = 0;
+    public Card card;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +23,13 @@ public class PawnMove : MonoBehaviour
         
     }
 
+    public void TurnStart()
+    {
+        card.DrawCards();
+    }
+
     public void Move(int MoveX, int MoveY) 
     {
-        
         transform.Translate(MoveX, MoveY, 0);
         Debug.Log("Pawn moved");
     }
